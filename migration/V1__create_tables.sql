14c0dcefc282e332efaf1c92052c38138754fb92
Parthvaghela8 <147900318+Parthvaghela8@users.noreply.github.com>
2024-02-18 19:49:46 +0530

    Create V1__create_tables.sql

---
+++ migration/V1__create_tables.sql
@@ -0,0 +1,13 @@
+-- Create a sample table
+CREATE TABLE SampleTable (
+    ID INT PRIMARY KEY,
+    Name NVARCHAR(100)
+);
+
+-- Insert some sample data into the table
+INSERT INTO SampleTable (ID, Name)
+VALUES (1, 'John'),
+       (2, 'Jane'),
+       (3, 'Alice');
+
+DROP TABLE new
