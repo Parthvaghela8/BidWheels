9ad19cb1781db5fb44d3d44688257178be54025e
vinayak tiwari <vinayaktiwari10301@gmail.com>
2024-02-19 01:25:43 +0530

    Scaler

---
+++ migrations/V3__Scaler.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION dbo.GetAverageRatingOfShipper (@ShipperID INT)
+RETURNS DECIMAL(5, 2)
+AS
+BEGIN
+    DECLARE @AverageRating DECIMAL(5, 2);
+
+    SELECT @AverageRating = AVG(CAST(Rating AS DECIMAL(5, 2)))
+    FROM Feedback
+    WHERE ShipperID = @ShipperID;
+
+    RETURN ISNULL(@AverageRating, 0); -- Return 0 if no rating is found for the shipper
+END;
+
+--SELECT dbo.GetAverageRatingOfShipper(1) AS AverageRatingForShipper1;
