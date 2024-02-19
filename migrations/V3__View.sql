6f8a89b05be4e19675bb7a8ad640200df5c6b221
parthvaghela <parthvaghela888@gmail.com>
2024-02-20 02:29:40 +0530

    insert

--- migrations/V3__View.sql
+++ migrations/V3__View.sql
@@ -1,23 +1,14 @@
-CREATE VIEW ShipmentDetails AS
+-- Create View
+CREATE VIEW CustomerShipments AS
 SELECT
+    C.CustomerID,
+    C.CustomerContactNumber,
     S.ShipmentID,
     S.ShipmentDate,
+    S.ImageURL
     S.DeliveryDate,
-    S.MaxBidAmount,
-    S.BidStartTime,
-    S.BidEndTime,
-    S.ImageURL,
-    S.CategoryID,
-    S.Description,
-    S.ShipmentStatus,
-    A1.StreetAddress AS OriginStreet,
-    A1.City AS OriginCity,
-    A1.State AS OriginState,
-    A1.PostalCode AS OriginPostal,
-    A2.StreetAddress AS DestStreet,
-    A2.City AS DestCity,
-    A2.State AS DestState,
-    A2.PostalCode AS DestPostal
-FROM Shipments S
-JOIN Address A1 ON S.OriginAddressID = A1.AddressID
-JOIN Address A2 ON S.DestinationAddressID = A2.AddressID;
+    S.Description AS ShipmentDescription,
+FROM
+    Customers C
+JOIN
+    Shipments S ON C.CustomerID = S.CustomerID;
